da2e53b00081315b4322649c02d5d0ae9ca3e7ae
Michael Abrams <michael.abrams@pendo.io>
2024-12-18 14:13:39 -0500

    Updated celebrate url

--- js/initPendo.js
+++ js/initPendo.js
@@ -4,17 +4,4 @@
         o[m]=o[m]||function(){o._q[m===v[0]?'unshift':'push']([m].concat([].slice.call(arguments,0)));};})(v[w]);
         y=e.createElement(n);y.async=!0;y.src='https://cdn.pendo-atlas.pendo-dev.com/agent/static/'+apiKey+'/pendo.js';
         z=e.getElementsByTagName(n)[0];z.parentNode.insertBefore(y,z);})(window,document,'script','pendo');
-
-        // This function creates visitors and accounts in Pendo
-        // You will need to replace <visitor-id-goes-here> and <account-id-goes-here> with values you use in your app
-        // Please use Strings, Numbers, or Bools for value types.
-        pendo.initialize({
-            visitor: {
-                id: 'michael.abrams@pendo.io'
-            },
-
-            account: {
-                id: 'pendo'
-            }
-        });
 })('a01f5fae-0fd2-4508-4483-49958681b392');
