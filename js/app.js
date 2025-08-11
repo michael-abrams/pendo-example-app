614f22434cb2fdd0bee8d92009dac9ecce8ce68e
Michael Abrams <michael.abrams@pendo.io>
2025-08-11 14:02:51 -0400

    Deploy

--- js/app.js
+++ js/app.js
@@ -16,7 +16,8 @@ function login(userInfo = {}) {
         visitor: {
             id: email, // Required if user is logged in
             email: email, // email:        // Optional // role:         // Optional // You can add any additional visitor level key-values here, // as long as it's not one of the above reserved names.
-            role: "ban"
+            role: "ban",
+            url: "https://www.google.com"
         },
         account: {
             id: department, // Highly recommended
